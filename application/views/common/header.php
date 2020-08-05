5c2d8f866aba6c85fee487abb8a07166ab04bd61
Buddhimal Gunasekara <bnawgunasekara@gmail.com>
2020-08-05 12:46:49 +0530

    Chnage Header

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -263,3 +263,55 @@
 		</div>
 	</div>
 </nav>
+
+<main>
+	<header class="header section text-contrast overlay overlay-primary alpha-7 parallax cover"
+			style="background-image: url(<?php echo base_url()?>assets/img/bg/sunrise.jpg);">
+		<div class="container-fluid pb-9">
+			<div class="row text-center">
+				<div class="col-12 col-lg-6 mx-auto"><img src="img/logo-light.png" alt="">
+					<h1 class="text-contrast bold display-4 display-md-3 text-capitalize mt-5">Meet DashCore</h1>
+					<p class="lead bold">Powerful Responsive SaaS, Startup & Software Template</p>
+					<nav class="nav d-flex justify-content-center mb-5 mt-7"><a
+							href="https://themeforest.net/item/dashcore-saas-startup-software-template/22397137"
+							class="btn btn-rounded btn-success px-4 mr-2 mr-md-5">Buy now <i
+								class="fas fa-long-arrow-alt-right ml-2"></i></a> <a href="#demos"
+																					 class="btn btn-rounded btn-outline-light scrollto px-4 mr-md-4">Explore
+							demos</a> <a href="changelog.html" target="_blank"
+										 class="btn btn-rounded px-4 btn-contrast mt-2 mt-md-0">Review Changelog</a></nav>
+				</div>
+			</div>
+		</div>
+	</header>
+	<footer class="site-footer section text-center bg-light block bg-light edge top-left">
+		<div class="container pb-4">
+			<div class="section-heading"><h2 class="font-md">Stay updated</h2>
+				<p class="lead text-secondary">We are actively adding more and more features as well as improving those
+					already included. Please enter your email bellow to stay always up to date.</p></div>
+			<div class="row">
+				<div class="col-12 col-md-6 mx-auto overflow-hidden">
+					<form action="https://5studios.net/themes/dashcore/srv/subscribe.php" class="form"
+						  data-response-message-animation="slide-in-left" novalidate="novalidate">
+						<div class="input-group"><input type="email" name="Subscribe[email]"
+														class="form-control rounded-circle-left"
+														placeholder="Enter your email" required="">
+							<div class="input-group-append">
+								<button class="btn btn-rounded btn-primary" type="submit">Register</button>
+							</div>
+						</div>
+					</form>
+					<div class="response-message"><i class="fas fa-check-circle font-lg text-primary"></i>
+						<p class="font-md m-0">Thank you!</p>
+						<p class="bold message">You are now subscribed to our newsletter.</p></div>
+				</div>
+			</div>
+			<hr class="mt-5">
+			<div class="row align-items-center">
+				<div class="col-md-2 text-center mx-auto"><img src="img/logo.png" alt="" class="logo">
+					<p class="mt-2 mb-0 text-secondary small">© 2018 <a class="brand bold" target="_blank"
+																		href="https://5studios.net/">5studios.net</a>.
+						All Rights Reserved</p></div>
+			</div>
+		</div>
+	</footer>
+</main>
